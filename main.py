58807685c9c526f68ea14bbb8210659bbaa0322e
salavat-mj <salavat-mj@users.noreply.github.com>
2020-02-10 01:38:58 +0300

    fix main.py

--- main.py
+++ main.py
@@ -88,4 +88,5 @@ def main():
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger = logging.getLogger(__name__)
     main()
